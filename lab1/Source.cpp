18f85a2507c457a90c0814f563c8ec272d230040
Amir <amir.fayzullin@gmail.com>
2022-04-02 15:09:38 +0500

    tutorial 2

--- lab1/Source.cpp
+++ lab1/Source.cpp
@@ -1,18 +1,63 @@
-#include <gl/freeglut.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <cstdio>
 using namespace std;
 
-void RenderSceneCB() {
+GLuint VBO;
+
+void render() {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	glDisableVertexAttribArray(0);
+
 	glutSwapBuffers();
 }
 
-int main(int argc, char** argv) {
+void prepareVertices() {
+	using namespace glm;
+	tvec3<float> vertices[1];
+	vertices[0] = tvec3<float>(0.0f, 0.0f, 0.0f);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+}
+
+void initializeGlut(int argc, char **argv, void (*render)()) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(800, 600);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Tutorial 01");
-	glutDisplayFunc(RenderSceneCB);
-	glClearColor(100.0f, 0.0f, 0.0f, 0.0f);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow("Amir");
+	glutDisplayFunc(render);
+}
+
+
+bool initializeGlew() {
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		throw "error";
+	}
+}
+
+int main(int argc, char** argv) {
+	initializeGlut(argc, argv, render);
+	try {
+		initializeGlew();
+	}
+	catch (char* e) {
+		return 1;
+	}
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	prepareVertices();
 	glutMainLoop();
 }
