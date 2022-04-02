218705595ba85a1a4136bd741d2e2e6f370bd3a6
Amir <amir.fayzullin@gmail.com>
2022-04-02 14:07:49 +0500

    lesson 1
    .gitignore update

--- lab1/Source.cpp
+++ lab1/Source.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <gl/freeglut.h>
 using namespace std;
 
-int main() {
-	cout << "hh";
+void RenderSceneCB() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 01");
+	glutDisplayFunc(RenderSceneCB);
+	glClearColor(100.0f, 0.0f, 0.0f, 0.0f);
+	glutMainLoop();
 }
